841b7ff9e01ef291a43acd3a58e99e4fcdc1268e
WissamTarik <www.wissamtarik2021@gmail.com>
2025-07-18 14:54:47 +0300

    Video01

---
+++ Session01OOP/PointDemo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    //Video01
+    //What can be created in Struct
+    //1.Methods
+    //2.Attributes(Variables=Field)
+    //3.Property
+    //4.Events
+    internal struct PointDemo
+    {
+        public int X;
+        public int Y;
+        //Constructor: is a special method
+        //            1.Its name by struct name
+        //            2.Has no return type
+        //public Point()
+        //{
+
+        //}
+        //.Net 5.0 C#9.0
+        //.Net 6.0 C#10.0
+
+        public PointDemo()
+        {
+            X = 12;
+            Y = 12;
+        }
+        public PointDemo(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void PrintPoint()
+        {
+            Console.WriteLine($"({X},{Y})");
+        }
+        public override string ToString()
+        {
+            return $"({X} , {Y})";
+        }
+    }
+}
--- Session01OOP/Program.cs
+++ Session01OOP/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Video01 Struct
+            //Struct is a value type stored in STACK
+
+            //Employee
+            //Id,Name,Salary
+
+            //PointDemo
+            //PointDemo P01;
+            //Declare for object from type "PointDemo"
+            //P01:Object
+            //Allocate uninitalized 8 Bytes at STACK
+            //X=>Variable=Attribute
+            //Console.WriteLine(P01.X);=>using  uninitalized attribute
+
+            //P01.X = 12;
+            //P01.Y = 12;
+
+
+            //P01=new PointDemo();
+            //new
+            //It select the constructor
+
+            //Console.WriteLine(P01.X);
+            //Console.WriteLine(P01.Y);
+            // Console.WriteLine(P01);//Namespace Demo.PointDemo
+            // Console.WriteLine(P01.ToString());//Namespace Demo.PointDemo
+            //P01.PrintPoint();
+            //Console.WriteLine(P01.ToString());
+            //Console.WriteLine(P01);
+
+            #endregion
         }
     }
 }
