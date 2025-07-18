c3c9edff376235f17d2f77f0bc4c87e2c3cab49e
WissamTarik <www.wissamtarik2021@gmail.com>
2025-07-18 15:03:02 +0300

    Part01 Q1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01 
+            #region Q1
+            Point[] P01 = new Point[2];
+            for (int i = 0; i < P01.Length; i++)
+            {
+                P01[i] = new Point();
+                bool Flag;
+                do
+                {
+                    Console.Write($"Enter P01 {i + 1} X: ");
+                    Flag = double.TryParse(Console.ReadLine(), out double x);
+                    P01[i].X = x;
+                } while (!Flag);
+                do
+                {
+                    Console.Write($"Enter P01 {i + 1} Y: ");
+                    Flag = double.TryParse(Console.ReadLine(), out double y);
+                    P01[i].Y = y;
+                } while (!Flag);
+
+
+            }
+
+            double distance = Math.Abs(Math.Sqrt(Math.Pow(P01[0].X - P01[1].X, 2) + Math.Pow(P01[0].Y - P01[1].Y, 2)));
+
+            Console.WriteLine($"The distance between ({P01[0].X},{P01[0].Y}) and  ({P01[1].X},{P01[1].Y}) is {distance}");
+            #endregion
+            #endregion
         }
+
     }
 }
